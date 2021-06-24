22beef6cb45527457db5919e0435bae32ab7e17d
che-ri <rnjs1135@gmail.com>
2021-06-25 01:15:50 +0900

    [feat] search api 연결

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,48 +1,55 @@
-import { moviesApi } from 'api';
-import React from 'react';
-import HomePresenter from './HomePresenter';
+import { moviesApi } from "api";
+import React from "react";
+import HomePresenter from "./HomePresenter";
 
 export default class extends React.Component {
-  state = {
-    nowPlaying: null,
-    upcoming: null,
-    popular: null,
-    loading: true,
-    error: null,
-  };
+    state = {
+        nowPlaying: null,
+        upcoming: null,
+        popular: null,
+        loading: true,
+        error: null,
+    };
 
-  //호출해야될 api동작들이 많다면 따로 밖에서 함수를 만들어서 componentDidMount 함수 안에 this.함수명 으로 넣어주어도 된다!
-  async componentDidMount() {
-    try {
-      //비구조화할당 후 nowPlaying으로 명시하기!
-      const { data: { results: nowPlaying } } = await moviesApi.nowPlaying()
-      const { data: { results: upcoming } } = await moviesApi.upcoming()
-      const { data: { results: popular } } = await moviesApi.popular()
-      this.setState({
-        nowPlaying,
-        upcoming,
-        popular,
-      })
-    } catch {
-      this.setState({
-        error: "Can't find Movies information."
-      })
-    } finally {
-      this.setState({
-        loading: false
-      })
+    //호출해야될 api동작들이 많다면 따로 밖에서 함수를 만들어서 componentDidMount 함수 안에 this.함수명 으로 넣어주어도 된다!
+    async componentDidMount() {
+        try {
+            //비구조화할당 후 nowPlaying으로 명시하기!
+            const {
+                data: { results: nowPlaying },
+            } = await moviesApi.nowPlaying();
+            const {
+                data: { results: upcoming },
+            } = await moviesApi.upcoming();
+            const {
+                data: { results: popular },
+            } = await moviesApi.popular();
+            this.setState({
+                nowPlaying,
+                upcoming,
+                popular,
+            });
+        } catch {
+            this.setState({
+                error: "Can't find Movies information.",
+            });
+        } finally {
+            this.setState({
+                loading: false,
+            });
+        }
     }
-  }
-  render() {
-    const { nowPlaying, upcoming, popular, error, loading } = this.state;
-    return (
-      < HomePresenter
-        nowPlaying={nowPlaying}
-        upcoming={upcoming}
-        popular={popular}
-        error={error}
-        loading={loading}
-      />
-    );
-  }
+    render() {
+        console.log(this.state);
+        const { nowPlaying, upcoming, popular, error, loading } = this.state;
+        return (
+            <HomePresenter
+                nowPlaying={nowPlaying}
+                upcoming={upcoming}
+                popular={popular}
+                error={error}
+                loading={loading}
+            />
+        );
+    }
 }
--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,54 +1,71 @@
-import { moviesApi, tvApi } from 'api';
-import React from 'react';
-import SearchPresenter from './SearchPresenter';
-
+import React from "react";
+import SearchPresenter from "./SearchPresenter";
+import { moviesApi, tvApi } from "../../api";
 export default class extends React.Component {
-  state = {
-    moviesResults: null,
-    tvResults: null,
-    //searchTerm에 입력값이 담길 것이다.
-    searchTerm: '',
-    loading: false,
-    error: null,
-  };
+    state = {
+        movieResults: null,
+        tvResults: null,
+        //searchTerm은 입력값이 담길 곳이다.
+        searchTerm: "",
+        loading: false,
+        error: null,
+    };
 
-  handleSubmit = () => {
-    const { searchTerm } = this.state;
-    //만약에 검색내용이 빈 문자열이 아니라면 searchByTerm 함수 실행
-    if (searchTerm !== "") this.searchByTerm()
-  }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const { searchTerm } = this.state;
+        //만약에 검색내용이 빈 문자열이 아니라면 searchByTerm 함수 실행
+        if (searchTerm !== "") {
+            this.searchByTerm();
+        }
+    };
 
-  searchByTerm = async () => {
-    const { searchTerm } = this.state;
-    try {
-      this.setState({ loading: true })
+    updateTerm = (event) => {
+        const {
+            target: { value },
+            //기존 이벤트 객체 안에 target:{value} 안에 input의 value가 담겨있다.
+        } = event;
+        this.setState({
+            searchTerm: value,
+            //value를 받아서 searchTerm에 저장한다.
+        });
+    };
 
-      // api.search()를 통해 searchTerm을 포함한 결과를 담습니다.
-      const { data: { results: movieResults } } = await moviesApi.search(searchTerm)
-      const { data: { results: tvResults } } = await tvApi.search(searchTerm)
-
-      //결과값을 state에 저장합니다.
-      this.setState({ movieResults, tvResults })
-
-    } catch {
-      this.setState({ error: "Can't find result." })
-    } finally {
-      this.setState({ loading: false })
+    searchByTerm = async () => {
+        const { searchTerm } = this.state;
+        this.setState({ loading: true });
+        // api.search()를 통해 searchTerm을 포함한 결과를 담습니다.
+        try {
+            const {
+                data: { results: movieResults },
+            } = await moviesApi.search(searchTerm);
+            const {
+                data: { results: tvResults },
+            } = await tvApi.search(searchTerm);
+            //결과값을 state에 저장합니다.
+            this.setState({
+                movieResults,
+                tvResults,
+            });
+        } catch {
+            this.setState({ error: "Can't find results." });
+        } finally {
+            this.setState({ loading: false });
+        }
+    };
+    render() {
+        const { movieResults, tvResults, searchTerm, loading, error } =
+            this.state;
+        return (
+            <SearchPresenter
+                movieResults={movieResults}
+                tvResults={tvResults}
+                loading={loading}
+                error={error}
+                searchTerm={searchTerm}
+                handleSubmit={this.handleSubmit}
+                updateTerm={this.updateTerm}
+            />
+        );
     }
-  }
-
-
-  render() {
-    const { moviesResults, tvResults, searchTerm, error, loading } = this.state;
-    return (
-      <SearchPresenter
-        moviesResults={moviesResults}
-        tvResults={tvResults}
-        searchTerm={searchTerm}
-        error={error}
-        loading={loading}
-        handleSubmit={this.handleSubmit}
-      />
-    );
-  }
 }
